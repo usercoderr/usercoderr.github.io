9d28c3cd409e5e8f311172207ee0d65ba1c6d6be
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-07-11 09:57:02 +0600

    v1.0

---
+++ precache-manifest.c6c321efc96b4425ab3b7a8b010e1926.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "704e2d7aa7d3d80cc053",
+    "url": "/static/css/main.b4de2df9.chunk.css"
+  },
+  {
+    "revision": "704e2d7aa7d3d80cc053",
+    "url": "/static/js/main.704e2d7a.chunk.js"
+  },
+  {
+    "revision": "1f3274c1812aa4d63521",
+    "url": "/static/js/1.1f3274c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9f2e5474707994a456d1a965736704d",
+    "url": "/static/media/BRABU1S.e9f2e547.png"
+  },
+  {
+    "revision": "0ff194a36be4bf052565355760ab89b2",
+    "url": "/static/media/Alivio.0ff194a3.png"
+  },
+  {
+    "revision": "c26210e26ded6acd11636011d272d82b",
+    "url": "/static/media/Levi-s.c26210e2.png"
+  },
+  {
+    "revision": "3503e958fae158a387a82ef4c53a6078",
+    "url": "/static/media/Mercedes-Benz.3503e958.png"
+  },
+  {
+    "revision": "95728bcfcfef53f238b2b6b6bece2dad",
+    "url": "/static/media/React-App.95728bcf.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6439303f21f2538f3d8eba344d7e4bc8",
+    "url": "/index.html"
+  }
+];
